40020022c41016dd6bdc035b5b42214309288d11
Calvin Tirrell <Calvin@Calvins-MacBook-Pro.local>
2015-12-14 13:09:30 -0800

    Added project

---
+++ lightHouseLabsFizzBuzz/lightHouseLabsFizzBuzz/main.c
@@ -0,0 +1,24 @@
+//  main.c
+//  lightHouseLabsFizzBuzz
+//
+//  Created by Calvin Tirrell on 12/14/15.
+//  Copyright © 2015 CT. All rights reserved.
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    int x;
+    
+    for (x = 1; x <= 100; x++) {
+        if ((x%3 == 0) && (x%5 == 0)) {
+            printf("FizzBuzz\n");
+        } else if (x%3 == 0) {
+            printf("Fizz\n");
+        } else if (x%5 == 0) {
+            printf("Buzz\n");
+        } else {
+            printf("%d\n",x);
+        }
+    }
+    return 0;
+}
